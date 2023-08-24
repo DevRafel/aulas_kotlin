b24c179f6a905b3820c1f190d90807ec1649f069
Rafael Lima <rafaelmoreiralimap4@gmail.com>
2023-08-24 16:45:33 -0300

    first commit

---
+++ src/main/kotlin/Main.kt
@@ -0,0 +1,12 @@
+fun main(args: Array<String>) {
+    // println("Hello World!")
+
+    //Mutável
+    var nome = "Rafael Moreira Lima"
+
+    // Imutável
+    val idade = 22
+
+
+    println(idade)
+}
