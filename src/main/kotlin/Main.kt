595fb39b0d2b6fca2ff8a168185b57e45aa32568
Rafael Lima <rafaelmoreiralimap4@gmail.com>
2023-08-24 17:09:34 -0300

    Tipos de Variáveis #02

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,12 +1,36 @@
 fun main(args: Array<String>) {
     // println("Hello World!")
+    /*
+    String - Textos
+    byte - Números Inteiros (-128 até 127)
+    int - Números Inteiros(-2147483648 até 2147483647)
+    long - Números Inteiros (-9223372036854775808 até 9223372036854775807)
+    floot - Números Decimais (1.1234567 até 7 casa decimais)
+    douple - Números Decimais (1.123456789 até 15 casas decimais)
+    boolean - Valores Booleanos (Estados) - true ou false
+     */
 
-    //Mutável
+
+    val nomeCompleto = "Rafel Moreira Lima"
+
+    /* Para colocar um valor a variavel (Byte e o unico que o kotlin não consege definir automatico!!)
+
+    val idade: Byte = 13
+     */
+
+    println(nomeCompleto)
+
+
+
+
+
+    /*Mutável
     var nome = "Rafael Moreira Lima"
 
-    // Imutável
+    Imutável
     val idade = 22
+   */
 
 
-    println(idade)
+
 }
