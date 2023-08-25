ce5c606d1078e8871c587978c2f7934aff0ebf54
Rafael Lima <rafaelmoreiralimap4@gmail.com>
2023-08-24 21:17:09 -0300

    Operadores Relacionais

--- src/main/kotlin/aula4.kt
+++ src/main/kotlin/aula4.kt
@@ -3,5 +3,8 @@ fun main (array: Array<String>){
     var numero1 = 20
     var numero2 = 10
 
-    println(numero1 + numero2)
+   // println(numero1 + numero2)
+   // println(numero1 == numero2)
+
+
 }
